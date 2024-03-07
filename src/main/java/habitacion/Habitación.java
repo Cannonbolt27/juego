3c413e3456d1bb7ba271c64568588c5d27725626
Diurno <Diurno@C08PC03.informatica.comercio>
2024-03-07 12:54:23 +0100

    Commit prueba

---
+++ src/main/java/habitacion/Habitación.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package habitacion;
+
+/**
+ *
+ * @author Diurno
+ */
+public class Habitación {
+    private String habitacion;
+//    private Habitacion hNorte;
+//    private Habitacion hSur;
+//    private Habitacion hEste;
+//    private Habitacion hOeste;
+}
